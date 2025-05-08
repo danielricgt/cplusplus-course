ab5defa7d62e8f2b30439046c0dc2469ab387317
Daniel galvan <danielricardogt@gmail.com>
2025-05-07 23:06:27 -0500

    :construction: arrays part II

--- Bloque5/Modulo5_3.cpp
+++ Bloque5/Modulo5_3.cpp
@@ -1,20 +1,18 @@
 #include <iostream>
 using namespace std;
 
-// int average (){
-//     return 0;
-// }
-
-// int arrayValue(){
-//     int numbers [10];
-// }
-
-int main () {
+int main()
+{
     int numbers[10] = {};
-    cout << "insert ten numbers" << endl;   
-    cin >> numbers[0] >>numbers[1] >> numbers[2] >> numbers[3] >> numbers[4] >> numbers[5] >> numbers[6] >> numbers[7] >> numbers[8] >> numbers[9];
-    cout << "the numbers are: " <<  numbers[0] << numbers [1] << endl;
-
+    cout << "insert ten numbers" << endl;
+    cin >> numbers[0] >> numbers[1] >> numbers[2] >> numbers[3] >> numbers[4] >> numbers[5] >> numbers[6] >> numbers[7] >> numbers[8] >> numbers[9];
+    cout << "the numbers are: " << endl;
+    for (int i = 0; i <= 9; i++)
+    {
+        cout << numbers[i] << endl;
+    }
+    cout << "the sum of the values is: " << numbers[0] + numbers[1] + numbers[2] + numbers[3] + numbers[4] + numbers[5] + numbers[6] + numbers[7] + numbers[8] + numbers[9] << endl;
+    
 
     return 0;
 }
