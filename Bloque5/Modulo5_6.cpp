e2541739392e88379634c5d8fac0e393095da356
Daniel galvan <danielricardogt@gmail.com>
2025-05-19 22:45:00 -0500

    :construction: char chain part II

--- Bloque5/Modulo5_6.cpp
+++ Bloque5/Modulo5_6.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 using namespace std;
 
+int main()
+{
 
-int main () {
-
-    char saludo [] = "HOLA";
+    char saludo[] = "HOLA";
     cout << saludo << endl;
 
-    char saludo2 [5] = {'H','o','l','a','\0'};
+    char saludo2[5] = {'H', 'o', 'l', 'a', '\0'};
     cout << saludo2 << endl;
 
     // concatenate char
@@ -21,7 +21,51 @@ int main () {
     {
         cout << "Nombre completo" << endl;
     }
-    
+    cout << "El nombre es:" << endl;
+     int contador = 0 ;
+     string nombre2;
+    for (int i = 0; i < nombre.length(); i++)
+    {
+        cout << nombre[i];
+        switch (nombre[i])
+
+        {
+        case 'a':
+            contador = contador + 1;
+            break;
+
+        case 'e':
+            contador = contador + 1;
+            break;
+
+        case 'i':
+            contador = contador + 1;
+            break;
+
+        case 'o':
+            contador = contador + 1;
+            break;
+
+        case 'u':
+            contador = contador + 1;
+            break;
+
+        default:
+            break;
+        }
+
+        if (nombre[i] == ' ')
+        {
+            nombre[i] = '-';
+        }
+        
+       
+    }
+    cout << "este nombre contiene: " << contador << " vocales" << endl;
+
+    cout << "el nombre modificado es " << nombre << endl;
+
+    cout << endl;
 
     return 0;
 }
